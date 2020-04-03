d23d3d8820d7f3bb0c2ff609e7c97f10f669420d
root <root@helichopper>
2020-04-03 11:32:15 -0400

    upadte

--- setup.sh
+++ setup.sh
@@ -1,5 +1,10 @@
 #/bin/bash
 
+apt-get update
+apt-get install apache2
+apt install php libapache2-mod-php
+systemctl restart apache2
+
 if [ command -v tar ]
 then
 	tar -xzvf setupF.tar.gz
@@ -10,4 +15,5 @@ else
 	rm setupF.tar.gz
 fi
 
-
+chown -R $USER:$USER *
+echo Done.
